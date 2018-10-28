783bd44c986d094b3df1d752e39f90ac7bc8fc22
tobylunt <toby.lunt@gmail.com>
2018-10-28 11:03:03 -0500

    separate js from html

--- map/views.py
+++ map/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView # new 
+from django.shortcuts import render_to_response, RequestContext # from https://stackoverflow.com/questions/15491727/include-css-and-javascript-in-my-django-template
 
 # Create your views here.
 class HomePageView(TemplateView): # from https://djangoforbeginners.com/pages-app/
