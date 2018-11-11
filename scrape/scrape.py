689a65affc16f485b8bee40f2c0e203c8976ea6a
tobylunt <toby.lunt@gmail.com>
2018-11-11 15:44:22 -0600

    clean up API request methods, retrieve headshot links

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -1,45 +1,128 @@
 # scrapes senatorial contribution data.
-# by: toby lunt
+# toby lunt
+# free to use
 
-# imports
+
+############
+# PREAMBLE #
+############
+
+# import libraries
 import urllib
 import json
 import requests
 from requests.auth import HTTPDigestAuth
 
-# open apikey file
+# open untracked apikey file, save the stored key from the top line
 with open('os_apikey.txt') as fp:
     lines = fp.read().split("\n")
 
-# set the api key into an object
-apikey = lines[0]
+# set up a currently empty handler for API call errors
+class CRPApiError(Exception):
+    """ Exception for CRP API errors """
 
-# attempt a scrape of the senate IDs for New Jersey
-url = 'https://www.opensecrets.org/api/?method=getLegislators&output=json&id=NJ&apikey=' + apikey
-myResponse = requests.get(url)
+# this code is modified from the Center for Responsive Politics python API, which is extremely helpful but not maintained.
+# https://github.com/opensecrets/python-crpapi
+# note that we also use some of the updates in unmerged pull requests in this repo, updated for python 3. thanks!
+class CRP(object):
 
-# check if we have an error code for our API request
-if(myResponse.ok):
+    # initialize API key as empty
+    apikey = None
 
-    # Loading the response data into a dict variable
-    # json.loads takes in only binary or string variables so using content to fetch binary content
-    # Loads (Load String) takes a Json file and converts into python data structure (dict or list, depending on JSON)
-    jData = json.loads(myResponse.content)
-    
-    # print properties
-    print("The response contains {0} properties".format(len(jData)))
-    print("\n")
-    
-    # print the full json object for inspection
-    print(json.dumps(jData, indent=4, sort_keys=True))
+    # define an API call method that concanates the URL
+    @staticmethod
+    def _apicall(func, params):
 
-# If response code is not ok (200), print the resulting http error code with description
-else:
-    myResponse.raise_for_status()
+        # check for API key
+        if CRP.apikey is None:
+            raise CRPApiError('Missing CRP apikey')
+
+        # compile URL - method, key, parameters
+        url = 'http://www.opensecrets.org/api/' \
+              '?method=%s&output=json&apikey=%s&%s' % \
+              (func, CRP.apikey, urllib.parse.urlencode(params))
+
+        # see if it works
+        try:
+            response = requests.get(url)
+            return response.json()['response']
+        except requests.exceptions.RequestException as e:  # This is the correct syntax
+            print(e)
+            sys.exit(1)
+            
+    class getLegislators(object):
+        @staticmethod
+        def get(**kwargs):
+            results = CRP._apicall('getLegislators', kwargs)['legislator']
+            return results
+        
+    class memPFDprofile(object):
+        @staticmethod
+        def get(**kwargs):
+            results = CRP._apicall('memPFDprofile', kwargs)['member_profile']
+            return results
+
+    class candSummary(object):
+        @staticmethod
+        def get(**kwargs):
+            result = CRP._apicall('candSummary', kwargs)['summary']
+            return result['@attributes']
+
+    class candContrib(object):
+        @staticmethod
+        def get(**kwargs):
+            results = CRP._apicall('candContrib', kwargs)['contributors']['contributor']
+            return results
+
+    class candIndustry(object):
+        @staticmethod
+        def get(**kwargs):
+            results = CRP._apicall('candIndustry', kwargs)['industries']['industry']
+            return results
+
+    class candSector(object):
+        @staticmethod
+        def get(**kwargs):
+            results = CRP._apicall('candSector', kwargs)['sectors']['sector']
+            return results
+
+    class candIndByInd(object):
+        @staticmethod
+        def get(**kwargs):
+            result = CRP._apicall('CandIndByInd', kwargs)['candIndus']
+            return result['@attributes']
+
+    class getOrgs(object):
+        @staticmethod
+        def get(**kwargs):
+            results = CRP._apicall('getOrgs', kwargs)['organization']
+            return results
+        
+    class orgSummary(object):
+        @staticmethod
+        def get(**kwargs):
+            results = CRP._apicall('orgSummary', kwargs)['organization']
+            return results
+        
+    class congCmteIndus(object):
+        @staticmethod
+        def get(**kwargs):
+            results = CRP._apicall('congCmteIndus', kwargs)['committee']['member']
+            return results
 
 
-# example api requests
-# https://www.opensecrets.org/api/?method=getLegislators&output=json&id=NJ&apikey=apikey
-# http://www.opensecrets.org/api/?method=getLegislators&id=NJ&apikey=apikey
-# https://www.opensecrets.org/api/?method=candContrib&cid=N00007360&cycle=2018&apikey=apikey
+# establish our api key within CRP
+CRP.apikey = lines[0]
 
+
+############
+# REQUESTS #
+############
+
+# begin with getting basic information about senators from CivilServiceUSA's repo
+url = "https://raw.githubusercontent.com/CivilServiceUSA/us-senate/master/us-senate/data/us-senate.json"
+r = requests.get(url).text
+
+# take a look at what we've got - full json
+parsed = json.loads(r)
+print(json.dumps(parsed, indent=4, sort_keys=True))
