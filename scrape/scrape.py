156cd3c810e9ddb0d422d779f862296ea661499a
tobylunt <toby.lunt@gmail.com>
2018-11-15 20:34:41 -0600

    headshots scrape and files, json index

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -11,11 +11,13 @@
 import urllib
 import json
 import requests
+import shutil
+import os
 from requests.auth import HTTPDigestAuth
 
 # open untracked apikey file, save the stored key from the top
 # line. save your API key into this file and adjust the path as needed.
-with open('../os_apikey.txt') as fp:
+with open('/Users/tobiaslunt/Documents/projects/senate_cash/os_apikey.txt') as fp:
     lines = fp.read().split("\n")
 
 # this code is modified from the Center for Responsive Politics python API, which is extremely helpful but not maintained.
@@ -32,20 +34,20 @@ class CRP(object):
 
     # initialize API key as empty
     apikey = None
-
+    
     # define an API call method that concanates the URL
     @staticmethod
     def _apicall(func, params):
-
+        
         # check for API key
         if CRP.apikey is None:
             raise CRPApiError('Missing CRP apikey')
-
+        
         # compile URL - method, key, parameters
         url = 'http://www.opensecrets.org/api/' \
               '?method=%s&output=json&apikey=%s&%s' % \
               (func, CRP.apikey, urllib.parse.urlencode(params))
-
+        
         # see if it works
         try:
             response = requests.get(url)
@@ -65,37 +67,37 @@ class CRP(object):
         def get(**kwargs):
             results = CRP._apicall('memPFDprofile', kwargs)['member_profile']
             return results
-
+        
     class candSummary(object):
         @staticmethod
         def get(**kwargs):
             result = CRP._apicall('candSummary', kwargs)['summary']
             return result['@attributes']
-
+        
     class candContrib(object):
         @staticmethod
         def get(**kwargs):
             results = CRP._apicall('candContrib', kwargs)['contributors']['contributor']
             return results
-
+        
     class candIndustry(object):
         @staticmethod
         def get(**kwargs):
             results = CRP._apicall('candIndustry', kwargs)['industries']['industry']
             return results
-
+        
     class candSector(object):
         @staticmethod
         def get(**kwargs):
             results = CRP._apicall('candSector', kwargs)['sectors']['sector']
             return results
-
+        
     class candIndByInd(object):
         @staticmethod
         def get(**kwargs):
             result = CRP._apicall('CandIndByInd', kwargs)['candIndus']
             return result['@attributes']
-
+        
     class getOrgs(object):
         @staticmethod
         def get(**kwargs):
@@ -135,9 +137,60 @@ print(json.dumps(parsed, indent=4, sort_keys=True))
 # our 50 states
 len(parsed)
 
-tmp = parsed[1]
-dict((k, tmp[k]) for k in ('state_code', 'name', 'opensecrets', 'photo_url'))
+# cut this down to only attributes we want to retain
 keys = ['state_code', 'name', 'opensecrets', 'photo_url']
-dict_short = { key: tmp[key] for key in keys }
+trunc = [{ key: parsed[i][key] for key in keys } for i in range(len(parsed))]
 
-[for i in range(len(parsed)) dict((k, tmp[i][k]) for k in ('state_code', 'name', 'opensecrets', 'photo_url'))]
+# inspect visually
+print(json.dumps(trunc, indent=4, sort_keys=True))
+
+# explore the API. select the first senator for a test run
+os = trunc[1]['opensecrets']
+
+# retrieve and examine sectoral contributions
+tmp = CRP.candSector.get(cid=os,cycle='2018')
+print(json.dumps(tmp, indent=4, sort_keys=True))
+
+# look at industry contributions, within sectors
+tmp = CRP.candIndustry.get(cid=os,cycle='2018')
+print(json.dumps(tmp, indent=4, sort_keys=True))
+
+# now look at top firm-level contributors.
+tmp = CRP.candContrib.get(cid=os,cycle='2018')
+print(json.dumps(tmp, indent=4, sort_keys=True))
+
+# we can see from this exploration that we can map sectors to
+# industries, but with insufficient detail, and cannot easily map top
+# contributors to industries or sectors. instead of the API, we'll
+# switch to the raw bulk data.
+
+# make a new directory to catch headshots
+headshotFolder = 'headshots'
+try:
+    os.mkdir(headshotFolder)
+except FileExistsError:
+    print("Directory " , headshotFolder ,  " already exists")
+
+# define a downloader
+def imgDownload(url, file_name):
+    # get request
+    response = requests.get(url, stream=True)
+    # write to file
+    with open(file_name, 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+    # remove the response
+    del response
+
+# save all of the headshots to disk
+p = [imgDownload(trunc[i]['photo_url'], headshotFolder + '/' + trunc[i]['opensecrets'] + '.jpg') for i in range(len(trunc))]
+del p
+
+# trim the json further - only need osid and state code in the web app
+keys = ['state_code', 'opensecrets']
+trunc = [{ key: parsed[i][key] for key in keys } for i in range(len(parsed))]
+
+# save opensecrets ID  and state code to a new json
+with open('map/static/headshots.json', 'w') as outfile:
+    json.dump(trunc, outfile)
+
+
