883450822a1bc88b19060dc9d30e7eb1e1e0f86b
tobylunt <toby.lunt@gmail.com>
2018-11-19 19:50:44 -0600

    postgres tables build 1.0

--- processing/process_data.sh
+++ processing/process_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# note: this script requires a 2-digit cycle year (e.g. 18)
+
 # exit on any non-zero status
 set -e
 
@@ -8,9 +10,31 @@ set -u
 
 # check for necessary input variables
 if [ $# != 2 ]; then
-    echo "please enter a db host and a table suffix"
+    echo "please enter 2-digit contribution cycle, db host and a table suffix"
     exit 1
 fi
+
+# get input vars into globals for readability (and for psql)
+export CYCLE=$1
+export TSUFF=$2
+
+# remove any previous iterations of the raw data, recreate the dir, and cd
+rm -rf raw
+mkdir raw
+cd raw
+
+# wget the zip, using the input election cycle, following CRP naming conventions
+wget --header="Host: s3.amazonaws.com" --header="User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36" --header="Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" --header="Accept-Language: en-US,en;q=0.9" "https://s3.amazonaws.com/assets3.opensecrets.org/bulk-data/CampaignFin$CYCLE.zip" -O "CampaignFin18.zip" -c
+
+# unzip the download
+unzip CampaignFin$CYCLE.zip
+
+# clean all input data to UTF8
+for file in *.txt
+do
+    iconv -f latin1 -t utf-8 "$file" > "$file.new" &&
+	mv -f "$file.new" "$file"
+done
 
 # start the postgres server
 pg_ctl -D /usr/local/var/postgres start
@@ -20,16 +44,7 @@ if psql -lqt | cut -d \| -f 1 | grep -qw contribs; then
 
     # if so, remove it and reinitialize so it can be rebuilt
     DROP DATABASE contribs;
-    CREATE DATABASE contribs;
-else
-
-    # if not, create it
-    CREATE DATABASE contribs;
 fi
-
-# get input vars into globals for psql
-export DBHOST=$1
-export TSUFF=$2
 
 # run the psql script
 psql \
--- processing/process_data.sql
+++ processing/process_data.sql
@@ -1,65 +1,218 @@
+-- much of this code has been adapted from Solomon's repo on getting opensecrets data into Postgres. thanks!
+-- https://github.com/Solomon/opensecrets_to_postgres
 
--- create a new table
---CREATE TABLE agency (
---    agency_id character varying,
---    agency_name character varying NOT NULL,
---    agency_url character varying NOT NULL,
---    agency_timezone character varying NOT NULL,
---    agency_lang character varying,
---    agency_phone character varying,
---    agency_fare_url character varying
---);
+-- turn on timing
+\timing
 
-CREATE TABLE "Pac_Other16" (
-    Cycle char(4) NOT NULL,
-    FECRecNo char(19) NOT NULL,
-    FilerID char(9) NOT NULL,
-    DonorCmte varchar(50) NULL,
-    ContribLendTrans varchar(50) NULL,
-    City varchar(30) NULL,
-    State char(2) NULL,
-    Zip char(5) NULL,
-    FECOccEmp varchar(38) NULL,
-    PrimCode char(5) NULL,
-    Date timestamp NULL,
-    Amount float NULL,
-    RecipID char(9) NULL,
-    Party char(1) NULL,
-    OtherID char(9) NULL,
-    RecipCode char(2) NULL,
-    RecipPrimcode char(5) NULL,
-    Amend char(1) NULL,
-    Report char(3) NULL,
-    PG char(1) NULL,
-    Microfilm char(11) NULL,
-    Type char(3) NULL,
-    Realcode char(5) NULL,
-    Source char(5) NULL
-)
-;
+-- create the contributions database and connect
+create database contribs;
+\c contribs
 
-
-CREATE TABLE "Pac_Other16" (
-    Cycle varchar(4) NOT NULL
+-- initialize the candidates table
+DROP TABLE candidates;
+CREATE TABLE candidates(
+  id bigserial primary key,
+  cycle varchar(255),
+  fec_cand_id varchar(255),
+  cid varchar(255),
+  first_last_party varchar(255),
+  party varchar(255),
+  dist_id_run_for varchar(255),
+  dist_id_currently_held varchar(255),
+  current_candidate varchar(255),
+  cycle_candidate varchar(255),
+  crpico varchar(255),
+  recip_code varchar(255),
+  nopacs varchar(255),
+  raised_from_pacs integer,
+  raised_from_individuals integer,
+  raised_total integer,
+  raised_unitemized integer
 );
 
+-- \copy depends on relative filepath - you need to be in /raw for this to work.
+\COPY candidates(cycle, fec_cand_id, cid, first_last_party, party, dist_id_run_for, dist_id_currently_held, current_candidate, cycle_candidate, crpico, recip_code, nopacs) from 'cands16.txt'  WITH CSV QUOTE '|' DELIMITER ',';
+
+# commitees data next
+DROP TABLE committees;
+CREATE TABLE committees(
+  id bigserial primary key,
+  cycle varchar(255),
+  committee_id varchar(255),
+  pac_short varchar(255),
+  affiliate varchar(255),
+  ultorg varchar(255),
+  recip_id varchar(255),
+  recip_code varchar(255),
+  fec_cand_id varchar(255),
+  party varchar(255),
+  prim_code varchar(255),
+  source varchar(255),
+  sensitive varchar(255),
+  foreign_owned varchar(255),
+  active integer
+);
+
+-- populate the commitees table
+\COPY committees(cycle, committee_id, pac_short, affiliate, ultorg, recip_id, recip_code, fec_cand_id, party, prim_code, source, sensitive, foreign_owned, active) from 'cmtes16.txt' WITH CSV QUOTE '|' DELIMITER ',';
+
+-- now for individual contribs
+DROP TABLE individual_contributions;
+CREATE TABLE individual_contributions(
+  id bigserial primary key,
+  cycle varchar(255),
+  fec_trans_id varchar(255),
+  contributor_id varchar(255),
+  contributor_name varchar(255),
+  recipient_id varchar(255),
+  org_name varchar(255),
+  ult_org varchar(255),
+  real_code varchar(255),
+  date timestamp,
+  amount integer,
+  street varchar(255),
+  city varchar(255),
+  state varchar(255),
+  zip varchar(255),
+  recip_code varchar(255),
+  type varchar(255),
+  committee_id varchar(255),
+  other_id varchar(255),
+  gender varchar(255),
+  old_format_employer_occupation varchar(255),
+  microfilm varchar(255),
+  occupation varchar(255),
+  employer varchar(255),
+  source varchar(255)
+);
+
+-- get data in from raw. largest table. 
+\COPY individual_contributions(cycle, fec_trans_id, contributor_id, contributor_name, recipient_id, org_name, ult_org, real_code, date, amount, street, city, state, zip, recip_code, type, committee_id, other_id, gender, microfilm, occupation, employer, source) from 'indivs16.txt' WITH CSV QUOTE '|' DELIMITER ',';
+
+-- pacs now
+DROP TABLE pacs;
+CREATE TABLE pacs(
+  id bigserial primary key,
+  cycle varchar(255),
+  fec_rec_no varchar(255),
+  pac_id varchar(255),
+  cid varchar(255),
+  amount integer,
+  date timestamp,
+  real_code varchar(255),
+  type varchar(255),
+  di varchar(255),
+  fec_cand_id varchar(255)
+);
+\COPY pacs(cycle, fec_rec_no, pac_id, cid, amount, date, real_code, type, di, fec_cand_id) from 'pacs16.txt' WITH CSV QUOTE '|' DELIMITER ',';
+
+-- pac to pacs
+DROP TABLE pac_to_pacs;
+CREATE TABLE pac_to_pacs(
+  id bigserial primary key,
+  cycle varchar(255),
+  fec_rec_no varchar(255),
+  filer_id varchar(255),
+  donor_committee varchar(255),
+  contrib_lend_trans varchar(255),
+  city varchar(255),
+  state varchar(255),
+  zip varchar(255),
+  fec_occ_emp varchar(255),
+  prim_code varchar(255),
+  date timestamp,
+  amount decimal,
+  recipient_id varchar(255),
+  party varchar(255),
+  other_id varchar(255),
+  recip_code varchar(255),
+  recip_prim_code varchar(255),
+  amend varchar(255),
+  report varchar(255),
+  pg varchar(255),
+  microfilm varchar(255),
+  type varchar(255),
+  real_code varchar(255),
+  source varchar(255)
+);
+\COPY pac_to_pacs(cycle, fec_rec_no, filer_id, donor_committee, contrib_lend_trans, city, state, zip, fec_occ_emp, prim_code, date, amount, recipient_id, party, other_id, recip_code, recip_prim_code, amend, report, pg, microfilm, type, real_code, source ) from 'pac_other16.txt' WITH CSV QUOTE '|' DELIMITER ',';
+
+---- industry codes - DONT HAVE THIS FILE YET.
+--DROP TABLE industry_codes;
+--CREATE TABLE industry_codes(
+--  id bigserial primary key,
+--  category_code varchar(255),
+--  category_name varchar(255),
+--  industry_code varchar(255),
+--  industry_name varchar(255),
+--  sector varchar(255),
+--  sector_long varchar(255)
+--);
+---- Line 443 extra column at end of line needs to be deleted
+--COPY industry_codes(category_code, category_name, industry_code, industry_name, sector, sector_long)
+--from '/Users/sik/Downloads/CRP_Categories.txt'
+--WITH CSV DELIMITER E'\t';
+
+-- politicians identifiers - from candidate table
+DROP TABLE politicians;
+CREATE TABLE politicians(
+  id bigserial primary key,
+  cid varchar(255),
+  name varchar(255)
+);
+
+-- load up the politician codes
+INSERT INTO politicians(cid, name)(
+  SELECT distinct on(cid) cid, left(first_last_party, -4)
+  FROM candidates
+);
+
+-- pac identifiers
+DROP TABLE pac_records;
+CREATE TABLE pac_records(
+  id bigserial primary key,
+  committee_id varchar(255),
+  pac_short varchar(255),
+  affiliate varchar(255),
+  ultorg varchar(255),
+  recip_id varchar(255),
+  recip_code varchar(255),
+  fec_cand_id varchar(255),
+  party varchar(255),
+  prim_code varchar(255),
+  source varchar(255),
+  sensitive varchar(255),
+  foreign_owned varchar(255)
+);
+
+-- load up pac identifiers from committees table
+INSERT INTO pac_records(committee_id, pac_short, affiliate, ultorg, recip_id, recip_code, fec_cand_id, party, prim_code, source, sensitive, foreign_owned)(
+  SELECT distinct on(committee_id)
+    committee_id, pac_short, affiliate, ultorg, recip_id, recip_code, fec_cand_id, party, prim_code, source, sensitive, foreign_owned
+  FROM committees
+);
+
+-- add desired indices to each of the tables
+CREATE INDEX ON candidates (cid);
+CREATE INDEX ON individual_contributions (recipient_id);
+CREATE INDEX ON individual_contributions (real_code);
+CREATE INDEX ON pacs (cid);
+CREATE INDEX ON pacs (real_code);
+--CREATE INDEX ON industry_codes (category_code);
+CREATE INDEX ON politicians (cid);
+CREATE INDEX ON politicians (name);
+CREATE INDEX ON individual_contributions (cycle);
+CREATE INDEX ON pacs (cycle);
+CREATE INDEX ON candidates (cycle);
+CREATE INDEX ON pac_records (committee_id);
 
 
--- query table info
-SELECT *
-FROM information_schema.columns
-WHERE table_schema = 'public'
-  AND table_name   = 'test_table';
-
--- another query
-select *
-from test_table
-where false;
-
-
--- copy from file on desktop
-COPY test_table FROM '/Users/tobiaslunt/Desktop/pac_other16.txt' WITH CSV QUOTE '|' DELIMITER ',';
 
 
 
+
+
+-- commit
+
+-- exit
 \q
