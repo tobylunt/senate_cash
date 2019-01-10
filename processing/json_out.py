674a3538f795926ee59bccb2b098c2a598806e71
tobylunt <toby.lunt@gmail.com>
2019-01-10 09:10:03 -0600

    working conversion from flattened table to deeply nested json

--- processing/json_out.py
+++ processing/json_out.py
@@ -1,35 +1,79 @@
 # imports
-import pandas as pd
+from collections import defaultdict
+import csv
 import json
 import os
+import pandas as pd
 
-# import csv - this is dumped from process_data.sql
-data = pd.read_csv("/Users/tobiaslunt/Documents/projects/senate_cash/processing/pre_json.csv") 
-data.head()
 
-# establish a function to recursively build a nested dict structure
+# establish functions to recursively build a nested dict structure
 # over columns, assuming values in rightmost column. need to make sure
 # the output JSON has the correct structure, where each nested feature
 # is named "children:"
-def jsonify(df):
-    json_out = dict()
-    ncols = df.values.shape[1]
-    for line in df.values:
-        d = json_out
-        for j, col in enumerate(line[:-1]):
-            if not col in d.keys():
-                if j != ncols-2:
-                    d[col] = {}
-                    d = d[col]
-                else:
-                    d[col] = line[-1]
-            else:
-                if j != ncols-2:
-                    d = d[col]
-    return json_out
+# see: https://stackoverflow.com/questions/43757965/convert-csv-to-json-tree-structure
 
-# run on our data
-j = jsonify(data)
+def ctree():
+    """ One of the python gems. Making possible to have dynamic tree structure.
+    
+    """
+    return defaultdict(ctree)
+
+
+def build_leaf(name, leaf):
+    """ Recursive function to build desired custom tree structure
+    
+    """
+    # need our parent categories under the "name" feature
+    res = {"name": name}
+    
+    # add "children" node if the leaf actually has any children
+    if len(leaf.keys()) > 0:
+        res["children"] = [build_leaf(k, v) for k, v in leaf.items()]
+        
+    return res
+
+
+def jsonify(fn):
+    """ The main thread composed from two parts.
+    
+    First it's parsing the csv file and builds a tree hierarchy from it.
+    Second it's recursively iterating over the tree and building custom
+    json-like structure (via dict).
+    
+    And the last part is just printing the result.
+    
+    """
+    tree = ctree()
+    
+    # insheet CSV file
+    with open(fn) as csvfile:
+        data = pd.read_csv(fn)
+        
+        # enumerate over csv, returning index and row/line
+        for rid, row in enumerate(data):
+            
+            # skipping first header row. remove this logic if your csv is
+            # headerless
+            if rid == 0:
+                continue
+            
+            # usage of python magic to construct dynamic tree structure and
+            # basically grouping csv values under their parents
+            leaf = tree[row[0]]
+            for cid in range(1, len(row)):
+                leaf = leaf[row[cid]]
+                
+    # building a custom tree structure
+    res = []
+    for name, leaf in tree.items():
+        res.append(build_leaf(name, leaf))
+        
+    # printing results into the terminal
+    return json.dumps(res)
+
+
+# process our table. this is dumped from process_data.sql
+j = jsonify('/Users/tobiaslunt/Documents/projects/senate_cash/processing/pre_json.csv')
 
 # dump to final json
 with open("/Users/tobiaslunt/Documents/projects/senate_cash/map/static/contribs.json","w") as f:
